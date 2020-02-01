0cf3ab8ce91f89644123c5b7ebc486f733515a35
Tristan Jaramillo-Pritchard <tristanjaramillopritchard@gmail.com>
2020-02-01 01:05:41 -0500

    Moved text

--- Super Pecan Pie/Super Pecan Pie/Form1.Designer.cs
+++ Super Pecan Pie/Super Pecan Pie/Form1.Designer.cs
@@ -35,18 +35,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(141, 205);
+            this.label1.Location = new System.Drawing.Point(132, 72);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(543, 91);
+            this.label1.Size = new System.Drawing.Size(865, 147);
             this.label1.TabIndex = 0;
             this.label1.Text = "This is text <3";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 703);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
