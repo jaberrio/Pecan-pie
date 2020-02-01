fa2cbc83fd72decf95eb42888e1851eb8954a64c
Julian Berrio <ja.berrio@ufl.edu>
2020-02-01 11:18:25 -0500

    Did stuff

---
+++ Super Pecan Pie/Super Pecan Pie/Accident .cs
@@ -0,0 +1,29 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Super_Pecan_Pie
+{
+    class Accident
+    {
+        public float Lat, Lon;
+
+        public Accident(float Lat, float Lon)
+        {
+            this.Lat = Lat;
+            this.Lon = Lon;
+
+            //0.01 degrees degrees = 1km
+        }
+
+        override public string ToString()
+        {
+            return Lat.ToString() + " , " + Lon.ToString();
+        }   
+        
+
+    }
+}
