49c322f6b3d656e9a8906a1245b257a9d0c004ca
pmarinelli18 <pmarinelli18@gmail.com>
2020-02-01 15:51:35 -0500

    Made tracking Available

--- Super Pecan Pie/Super Pecan Pie/Form1.cs
+++ Super Pecan Pie/Super Pecan Pie/Form1.cs
@@ -29,6 +29,9 @@ namespace Super_Pecan_Pie
 
         private void label1_Click(object sender, EventArgs e)
         {
+            
+
+            /*
             ImageViewer viewer = new ImageViewer();
             VideoCapture capture = new VideoCapture();
             Application.Idle += new EventHandler(delegate (object ss, EventArgs ee)
@@ -36,8 +39,14 @@ namespace Super_Pecan_Pie
                viewer.Image = capture.QueryFrame();
             });
             viewer.ShowDialog();
+            CascadeClassifier c = new CascadeClassifier("Files/cars.xml");*/
         }
 
+        private void vidFeed_Click(object sender, EventArgs e)
+        {
+            openCV f = new openCV();
+            f.findImages();
+        }
     }
     
 }
